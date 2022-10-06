a909ff52b8113235d9c19119f4a213607a76243e
Farzeen Kistnareddy <90398313+FarzeenKist@users.noreply.github.com>
2022-10-06 20:53:24 +0400

    Additional checks

--- src/contracts/animalhub_contract.py
+++ src/contracts/animalhub_contract.py
@@ -16,9 +16,18 @@ class AnimalHub:
     
     def application_creation(self):
         return Seq([
-            Assert(Txn.application_args.length() == Int(5)),
-            Assert(Txn.note() == Bytes("animal-hub:uv1")),
-            Assert(Btoi(Txn.application_args[3]) > Int(0)),
+            # checks if there are any empty values in application_args
+            Assert(
+                And(
+                    Txn.note() == Bytes("animal-hub:uv1"),
+                    Txn.application_args.length() == Int(5),
+                    Btoi(Txn.application_args[3]) > Int(0),
+                    Len(Txn.application_args[0]) > Int(0),
+                    Len(Txn.application_args[1]) > Int(0),
+                    Len(Txn.application_args[2]) > Int(0),
+                    Len(Txn.application_args[4]) > Int(0)
+                ),
+            ),
             App.globalPut(self.Variables.name, Txn.application_args[0]),
             App.globalPut(self.Variables.description, Txn.application_args[1]),
             App.globalPut(self.Variables.image, Txn.application_args[2]),
@@ -29,16 +38,24 @@ class AnimalHub:
             Approve()
         ])
 
-
+    # allow users to adopt an animal that is up for adoption
+    # user must also sends a payment transaction matching the amount required to adopt the animal
     def adopt(self):
         return Seq([
+            # checks if there are 2 transactions in this transaction group(adopt transaction and payment transaction)
+            # checks if the new owner(application_args[1]) is not an empty value
+            # checks if animal is up for adoption
             Assert(
                 And(
                     Global.group_size() == Int(2),
                     Txn.application_args.length() == Int(2),
-                    App.globalGet(self.Variables.adopted) == Int(0)
+                    Len(Txn.application_args[1]) > Int(0),
+                    App.globalGet(self.Variables.adopted) == Int(0),
+                    App.globalGet(self.Variables.address) != Txn.sender(),
                 ),
             ),
+            # checks if receiver of payment is the current animal's owner
+            # checks if the amount sent matches the amount required to adopt the animal
             Assert(
                 And(
                     Gtxn[1].type_enum() == TxnType.Payment,
@@ -56,11 +73,13 @@ class AnimalHub:
         ])
 
     def release(self):
+
+        # checks if it is the sender of the transaction is the owner of the animal
         Assert(
             And(
                 Txn.application_args.length() == Int(2),
-                App.globalGet(
-                    self.Variables.owner) == Txn.application_args[1]
+                App.globalGet(self.Variables.owner) == Txn.application_args[1],
+                App.globalGet(self.Variables.address) == Txn.sender(),
             ),
         )
 
